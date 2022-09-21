00d29569beddafe334b35efd152b97350b453f29
Kunal Prasad <65479883+KunalEveryWhere@users.noreply.github.com>
2022-09-21 17:36:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def index():
     # return 'This is the ML Python API for HaloBaby Project, under TTRI. \n edst. 2022 Team HaloBaby \n\nKindly check [/routes] for all route description'
     return '/ -> GET -> General Desciption of this API \n/routes -> GET -> All available routes and their description\n/emotionPredict -> POST -> Takes in an array of ECG Data, and returns the predicted emotional state\n/cryPredict -> POST -> Takes in an array of Mic Data, and returns the predicted reason of cry-state'
 
-# @app.route('/routes')
-# def index():
-#     return '/ -> GET -> General Desciption of this API \n/routes -> GET -> All available routes and their description\n/emotionPredict -> POST -> Takes in an array of ECG Data, and returns the predicted emotional state\n/cryPredict -> POST -> Takes in an array of Mic Data, and returns the predicted reason of cry-state'
+@app.route('/routes')
+def index():
+    return '/ -> GET -> General Desciption of this API \n/routes -> GET -> All available routes and their description\n/emotionPredict -> POST -> Takes in an array of ECG Data, and returns the predicted emotional state\n/cryPredict -> POST -> Takes in an array of Mic Data, and returns the predicted reason of cry-state'
 
 
 @app.route('/emotionPredict',methods=['POST'])
